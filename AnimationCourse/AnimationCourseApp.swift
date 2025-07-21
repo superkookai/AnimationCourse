65a05682603ea5124317566a7b61e19e9995f9c5
superkookai <weerawut@live.com>
2025-07-21 17:58:35 +0700

    Initial Commit

---
+++ AnimationCourse/AnimationCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationCourseApp.swift
+//  AnimationCourse
+//
+//  Created by Weerawut Chaiyasomboon on 21/07/2568.
+//
+
+import SwiftUI
+
+@main
+struct AnimationCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
